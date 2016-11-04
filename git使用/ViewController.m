c2f6960cacd1d5b0631e4d4a61b70234adfd1282
chenkaijie <chenkj@hadlinks.com>
2016-11-04 20:08:57 +0800

    增加文件

---
+++ git使用/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  git使用
+//
+//  Created by chenkaijie on 16/11/4.
+//  Copyright © 2016年 chenkaijie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"1 ");
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
